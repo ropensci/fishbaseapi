d4c625a4846406f8b5eed31a85fb0aaf7478cdea
Carl Boettiger <cboettig@gmail.com>
2015-02-09 14:37:54 -0500

    add command to initialize the database

--- docker.sh
+++ docker.sh
@@ -5,8 +5,23 @@
 # Note that ropensci/fishbaseapi is built by DockerHub auotmated build. 
 
 docker run --name fbredis -d redis:latest
+docker run --name fbmysql -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
 
-docker run --name fbmysql -d -v /home/cboettig/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
+## Import the database if we don't have it.
+## Assumes the database is called fbapp.sql and is in the working directory
+if [ ! -d "$HOME/data/fishbase" ]
+then 
+  mkdir -p $HOME/data/fishbase
+  docker run --rm -ti --link fbmysql:mysql \
+    -v ${PWD}/fbapp.sql:/data/fbapp.sql \
+    -w /data mysql mysql \
+    --host=$MYSQL_PORT_3306_TCP_ADDR \
+    --protocol=$MYSQL_PORT_3306_TCP_PROTO \
+    --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD \
+    fbapp < fbapp.sql
+fi
+
+
 
 # Give the sql database a few seconds to start up first. 
 # Perhaps the api.rb could be convinced to re-attempt `Client.new` at intervals if the mysql server isn't there

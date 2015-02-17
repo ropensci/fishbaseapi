b64ca4157bba4ff61f2980c7390c7d92f283120d
Carl Boettiger <cboettig@gmail.com>
2015-02-16 23:16:29 -0500

    avoid the weird --net call and link the nginx container properly.

--- docker.sh
+++ docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run --name fbredis -d redis:latest
-
 docker run --name fblogstash -d \
   -v /root \
 	-p 9292:9292 \
@@ -10,18 +9,8 @@ docker run --name fblogstash -d \
 	pblittle/docker-logstash
 
 docker run --name fbmysql --restart=always -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
+docker pull ropensci/fishbaseapi:latest
 
-# Make sure we have the latest version by pulling or building
-#docker pull ropensci/fishbaseapi
-docker build -t ropensci/fishbaseapi .
-
-# Give the sql database a few seconds to start up first.
-sleep 2
-
-
-## Start fb app, but expose only the nginx port (port 80), not the unicorn port (8080)
-docker run --name fbapi -d -p 80:80 --link fbmysql:mysql --link fbredis:redis --volumes-from fblogstash ropensci/fishbaseapi
-
-## NOTE: Sever name must be hardwired into nginx.conf. Please adjust appropriately
-docker run --name fbnginx -d --net container:fbapi -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf nginx
+docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --volumes-from fblogstash ropensci/fishbaseapi:latest
+docker run --name fbnginx -d -p 80:80 --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf nginx:latest
 

561a10a056a86bd6a8874579e47f039159473f0d
Carl Boettiger <cboettig@gmail.com>
2015-02-18 15:02:30 -0500

    debugging logging

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker rm -f fbredis fbmysql fbapi fblogstash fbnginx
 
 docker run --name fbredis -d redis:latest
 docker run --name fblogstash -d \
@@ -9,7 +10,7 @@ docker run --name fblogstash -d \
 	pblittle/docker-logstash
 
 docker run --name fbmysql --restart=always -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
-docker pull ropensci/fishbaseapi:latest
+docker build -t  ropensci/fishbaseapi:latest .
 
 docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --volumes-from fblogstash ropensci/fishbaseapi:latest
 docker run --name fbnginx -d -p 80:80 --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf nginx:latest

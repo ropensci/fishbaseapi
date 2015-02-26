176fee7f693ad5eb6dce127a53f9bb10ed9eee5c
Carl Boettiger <cboettig@gmail.com>
2015-02-26 17:53:10 -0500

    working on secure logstash, but nginx proxy gives 502 after authentication

--- docker.sh
+++ docker.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 docker rm -f -v fbredis fbmysql fbapi fblogstash fbnginx
-
 docker run --name fbredis -d redis:latest
 docker run --name fblogstash -d \
   -p 9292:9292 \
+  -e LOGSTASH_CONFIG_URL=https://raw.githubusercontent.com/ropensci/fishbaseapi/master/logstash.conf \
   -v /var/log/fishbase \
-  -v ${PWD}/logstashconf:/opt/logstash/conf.d \
 	pblittle/docker-logstash
-
-# -e LOGSTASH_CONFIG_URL=https://raw.githubusercontent.com/ropensci/fishbaseapi/master/logstash.conf \
+##  -v ${PWD}/logstashconf:/opt/logstash/conf.d \
 
 docker run --name fbmysql --restart=always -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
-docker build -t  ropensci/fishbaseapi:latest .
+#docker build -t  ropensci/fishbaseapi:latest .
 
 docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --volumes-from fblogstash ropensci/fishbaseapi:latest
-docker run --name fbnginx -d -p 80:80 --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf nginx:latest
+
+# Must generate a .htpassword file first:
+# assuming apache2-utils is installed: sudo htpasswd -cb .htpasswd $USER $PASSWORD
+
+docker run --name fbnginx -d -p 80:80 -p 81:81 --link fblogstash:logstash --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf -v ${PWD}/.htpasswd:/etc/nginx/.htpasswd nginx:latest
 

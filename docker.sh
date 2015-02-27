06513f02827d917d4d08b9de5c6d7a0448f570a0
Carl Boettiger <cboettig@gmail.com>
2015-02-27 10:34:06 -0800

    add geoip container

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker rm -f -v fbredis fbmysql fbapi fblogstash fbnginx
+docker rm -f -v fbredis fbmysql fbapi fblogstash fbnginx fbgeoip
 docker run --name fbredis -d redis:latest
 docker run --name fblogstash -d \
   -v /var/log/fishbase \
@@ -11,10 +11,12 @@ docker run --name fblogstash -d \
 docker run --name fbmysql --restart=always -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
 docker build -t  ropensci/fishbaseapi:latest .
 
-docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --volumes-from fblogstash ropensci/fishbaseapi:latest
+docker run --name fbgeoip -d allingeek/docker-freegeoip
+
+docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --link fbgeoip:geoip --volumes-from fblogstash ropensci/fishbaseapi:latest
 
 # Must generate a .htpassword file first:
 # assuming apache2-utils is installed: sudo htpasswd -cb .htpasswd $USER $PASSWORD
 
-docker run --name fbnginx -d -p 80:80 -p 9200:9200 -p 9292:9292 --link fblogstash:logstash --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf -v ${PWD}/.htpasswd:/etc/nginx/.htpasswd nginx:latest
+docker run --name fbnginx -d -p 80:80 --link fblogstash:logstash --link fbapi:api -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf -v ${PWD}/.htpasswd:/etc/nginx/.htpasswd nginx:latest
 

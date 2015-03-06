d9019a18886f42aa6ceaf6cf9cfc8c1bbda8e581
Carl Boettiger <cboettig@gmail.com>
2015-03-05 21:58:25 -0800

    add more endpoints

--- docker.sh
+++ docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker rm -f -v fbredis fbmysql fbapi fblogstash fbnginx fbgeoip
+
 docker run --name fbredis -d redis:latest
+
 docker run --name fblogstash -d \
   -v /var/log/fishbase \
   -v ${PWD}/logstashconf:/opt/logstash/conf.d \
@@ -9,10 +11,10 @@ docker run --name fblogstash -d \
 ## ##  -e LOGSTASH_CONFIG_URL=https://raw.githubusercontent.com/ropensci/fishbaseapi/master/logstash.conf \
 
 docker run --name fbmysql --restart=always -d -v $HOME/data/fishbase:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest
-docker build -t  ropensci/fishbaseapi:latest .
 
 docker run --name fbgeoip -d allingeek/docker-freegeoip
 
+docker build -t  ropensci/fishbaseapi:latest .
 docker run --name fbapi -d --link fbmysql:mysql --link fbredis:redis --link fbgeoip:geoip --volumes-from fblogstash ropensci/fishbaseapi:latest
 
 # Must generate a .htpassword file first:

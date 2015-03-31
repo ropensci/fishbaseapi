2df9de5e56e97d7786473a15bd3a6fae6cfbc09f
nexus-9 <cboettig@gmail.com>
2015-03-31 16:53:23 -0700

    update container name to avoid conflict

--- test-nginx.sh
+++ test-nginx.sh
@@ -5,7 +5,7 @@
 # Must generate a .htpassword file first:
 # assuming apache2-utils is installed: sudo htpasswd -cb .htpasswd $USER $PASSWORD
 
-docker run --name fbnginx -d \
+docker run --name nginx -d \
   -p 443:443 \
   -v ${PWD}/nginx-ssl.conf:/etc/nginx/nginx.conf \
   -v ${PWD}/.htpasswd:/etc/nginx/.htpasswd \
